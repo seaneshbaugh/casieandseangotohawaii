160072d32af7ef78eadc039328fae73b621566eb
Sean Eshbaugh <seaneshbaugh@gmail.com>
2017-08-14 18:00:31 -0500

    Oops

--- javascripts/application.js
+++ javascripts/application.js
@@ -218,7 +218,7 @@
   };
 
   const completed = function() {
-    setUIFromUIState(getUIStateFromQuery(parseQueryString(window.location.search) || defaultUIState));
+    setUIFromUIState(getUIStateFromQuery(parseQueryString(window.location.search) || {}));
 
     setEventHandlers();
   };
