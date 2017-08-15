b30385cf9cabf3604cf0c12699fbecb31efb4b8e
Sean Eshbaugh <seaneshbaugh@gmail.com>
2017-08-14 23:33:35 -0500

    Added deploy script.

---
+++ deploy.rb
@@ -0,0 +1,32 @@
+require 'aws-sdk'
+
+credentials = Aws::SharedCredentials.new(profile_name: ENV['AWS_CREDENTIALS_PROFILE'] || 'sean')
+
+client = Aws::S3::Client.new(credentials: credentials, region: ENV['AWS_REGION'] || 'us-east-1')
+
+bucket = ENV['S3_BUCKET'] || 'hawaii-2017'
+
+client.create_bucket(bucket: bucket)
+
+client.put_bucket_acl({
+  acl: 'public-read',
+  bucket: bucket
+})
+
+Dir[File.join(__dir__, '**', '*.{css,html,ico,jpg,js,png,svg}')].each do |file|
+  object_key = file.split(__dir__).last.sub(/^\//, '')
+
+  puts "Uploading #{object_key}"
+
+  client.put_object({
+    bucket: bucket,
+    key: object_key,
+    body: File.read(file)
+  })
+
+  client.put_object_acl({
+    acl: 'public-read',
+    bucket: bucket,
+    key: object_key
+  })
+end
